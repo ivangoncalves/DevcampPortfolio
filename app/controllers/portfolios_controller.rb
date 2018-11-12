5698f5d094bc4b76d4a52e113b14fdc0a15a89c1
Ivan Goncalves <ivangg@gmail.com>
2018-11-12 16:47:25 -0200

    added the destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -45,4 +45,12 @@ class PortfoliosController < ApplicationController
 
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Project was deleted.'}
+    end
+  end
+
 end
