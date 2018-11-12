214f5938e873544490db5fd9b902448c64bdc07a
Ivan Goncalves <ivangg@gmail.com>
2018-11-12 16:30:07 -0200

    implemented show functionality for personality module

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ puts "5 skills created"
     title: "Portfolio title #{portfolio_item}",
     subtitle: "My great service",
     body: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 
-    main_image: "placehold.it/600x400",
+    main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
   )
 end
