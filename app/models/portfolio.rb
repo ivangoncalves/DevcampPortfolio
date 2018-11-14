3e72a6f969a3a7e7b730bab8606d2bd465b4b6fc
Ivan Goncalves <ivangg@gmail.com>
2018-11-14 14:53:17 -0200

    Added angular page filter in agular.rb

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,16 @@ class Portfolio < ApplicationRecord
 
   after_initialize :set_defaults
 
+  # CUSTOM SCOPES
+  # prefered way
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  #second options is using scope
+  scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
+
+  # DEFAULTS
   def set_defaults
     self.main_image ||= "http://placehold.it/600x400"
     self.thumb_image ||= "http://placehold.it/350x200"
